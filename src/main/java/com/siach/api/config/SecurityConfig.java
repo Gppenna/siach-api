28b714ca3bdee9b7f291a82f5b5f17e389b4a299
Gabriel Lacerda Oliveira Penna <gabriel_600270@trt5.jus.br>
2022-10-21 00:46:53 -0300

    !bigday

--- src/main/java/com/siach/api/config/SecurityConfig.java
+++ src/main/java/com/siach/api/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().authorizeRequests()
-                .antMatchers("/login", "/register").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/adminRegister").hasAuthority("1")
                 .anyRequest().authenticated()
                 .and()
